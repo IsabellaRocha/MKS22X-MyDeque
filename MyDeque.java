c50e518b88a7091b535303b1dc9ef903e293f79a
Isabella Rocha <irocha00@stuy.edu>
2019-03-21 14:50:45 -0400

    Fixed adding and removing and toString

--- MyDeque.java
+++ MyDeque.java
@@ -1,44 +1,57 @@
 public class MyDeque<E> {
   private E[] data;
-  private int size, start, end;
-
+  private int size, start, end, check;
   public MyDeque(){
     @SuppressWarnings("unchecked")
     E[] d = (E[])new Object[10];
     data = d;
     size = 10;
-    start = 0;
-    end = 0;
+    start = data.length / 2;
+    end = data.length / 2;
+    check = 0;
   }
   public MyDeque(int initialCapacity){
     @SuppressWarnings("unchecked")
     E[] d = (E[])new Object[initialCapacity];
     data = d;
     size = initialCapacity;
-    start = 0;
-    end = 0;
+    start = data.length / 2;
+    end = data.length / 2;
+    check = 0;
   }
   public int size(){
     return size;
   }
   public String toString(){
     String output = "{";
-    for (int idx = 0; idx < data.length; idx++) {
-      output += data[idx] + " ";
+    if (end >= start) {
+      for (int idx = start; idx <= end; idx++) {
+        output += data[idx] + " ";
+      }
     }
+    else {
+      for (int idx = start; idx < data.length; idx++) {
+        output += data[idx] + " ";
+      }
+      for (int idx = 0; idx <= end; idx++) {
+        output += data[idx] + " ";
+      }
+    }
+    output += "}";
     return output;
   }
   public void addFirst(E element){
     if (start == 0) {
-      start = data.length;
+      start = data.length - 1;
     }
-    else {
+    else if (check != 0){
       start--;
     }
+    check = 1;
     data[start] = element;
   }
   public void addLast(E element){
-    if (end == data.length) {
+    if (end == data.length - 1) {
       end = 0;
     }
     else {
@@ -63,5 +76,14 @@ public class MyDeque<E> {
   }
   public E getLast(){
     return data[end];
+  }
+  public static void main(String[] args) {
+    MyDeque test = new MyDeque();
+    test.addLast(12);
+    test.addLast(12);
+    test.addLast(12);
+    test.addLast(12);
+    test.addFirst(17);
+    System.out.println(test);
   }
 }
